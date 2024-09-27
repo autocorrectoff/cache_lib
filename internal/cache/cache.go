ce0354113324f759cdf6bd2f2518697fce26aefd
LAPTOP-QLEGTRSJ\radio <branko.miric.ns@gmail.com>
2024-09-27 10:41:18 +0200

    refactor to use generics

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -5,23 +5,42 @@ import (
 	"sync"
 )
 
-type Cache struct {
+// LRU Cache
+type Cache[K comparable, V any] struct {
 	capacity int
-	items    map[string]*list.Element
-	ll       *list.List
 	mu       sync.Mutex
+	items    map[K]*list.Element
+	evict    *list.List
 }
 
-type entry struct {
-	key   string
-	value interface{}
+type entry[K comparable, V any] struct {
+	key   K
+	value V
 }
 
-func NewCache(capacity int) *Cache {
-	return &Cache{
+func NewCache[K comparable, V any](capacity int) *Cache[K, V] {
+	return &Cache[K, V]{
 		capacity: capacity,
-		items:    make(map[string]*list.Element),
-		ll:       list.New(),
+		items:    make(map[K]*list.Element),
+		evict:    list.New(),
 	}
 }
+
+func (c *Cache[K, V]) Set(key K, value V) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Check if the item already exists and update and move item to the front of the eviction list
+	if el, ok := c.items[key]; ok {
+		el.Value.(*entry[K, V]).value = value
+		c.evict.MoveToFront(el)
+		return
+	}
+
+	// Insert the new item
+	ent := &entry[K, V]{key, value}
+	el := c.evict.PushFront(ent)
+	c.items[key] = el
+}
+
 
